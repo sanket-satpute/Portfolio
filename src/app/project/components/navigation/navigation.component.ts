7fc20efb29d7e8165c147646faf55955fe67d8bd
ssatp <ssatpute738@gmail.com>
2024-09-05 17:31:56 +0530

    added hamburger icon for mobile devices in navbar

--- src/app/project/components/navigation/navigation.component.ts
+++ src/app/project/components/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent {
+  @ViewChild('overlay') overlay!: ElementRef;
 
+  isOverlayVisible = false;
+
+  toggleOverlay(show: boolean): void {
+    this.isOverlayVisible = show;
+    if (this.overlay) {
+      if (show) {
+        this.overlay.nativeElement.style.bottom = '0';
+        this.overlay.nativeElement.style.height = '100vh';
+      } else {
+        this.overlay.nativeElement.style.bottom = '100%';
+        this.overlay.nativeElement.style.height = '0';
+      }
+    }
+  }
+
+  onHamburgerClick(): void {
+    this.toggleOverlay(true);
+  }
+
+  onCrossButtonClick(): void {
+    this.toggleOverlay(false);
+  }
 }
