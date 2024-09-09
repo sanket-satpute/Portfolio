d10ba113170df190cc2df1c2377006f58d07af7e
ssatp <ssatpute738@gmail.com>
2024-09-09 18:06:48 +0530

    gave correct path of image for attach documents in hire-me dialog

--- src/app/project/components/dialogs/hire-me-dialog/hire-me-dialog.component.ts
+++ src/app/project/components/dialogs/hire-me-dialog/hire-me-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'app-hire-me-dialog',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./hire-me-dialog.component.css']
 })
 export class HireMeDialogComponent {
-
+  constructor(protected appTs: AppComponent) {}
 }
